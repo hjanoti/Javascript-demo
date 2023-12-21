031fedb44e197a2f2500e45e8698f73c91f09e80
Heera singh janoti <141156362+hjanoti@users.noreply.github.com>
2023-12-21 16:32:05 +0000

    basics of variable

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,4 +1,8 @@
 const accountId = 12345;
 let accountEmail = "hjanoti@1998@gmail.com";
 var accountPassword = "123456789";
-accountCity = "Jaipur";
+let accountCity;
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword]);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
