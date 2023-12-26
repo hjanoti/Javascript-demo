0187ab0637f8877570b350192d503f8dd0c98516
Heera singh janoti <141156362+hjanoti@users.noreply.github.com>
2023-12-26 15:12:42 +0000

    object

---
+++ 02_basics/object.js
@@ -0,0 +1,12 @@
+const myObj = {
+    name : "heera",
+    age : 24,
+    email : "hjanoti@gmail.com",
+    freeOn : ["mon", "tue", "wed"]
+}
+
+let mySmb = Symbol("Keys1");
+
+myObj.mySmb;
+
+console.log(myObj);
